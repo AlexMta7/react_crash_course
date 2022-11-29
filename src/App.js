3a4af324e8127498fd1b1dbca28ca4417f1d5d35
Alex <alexrobertomata@hotmail.com>
2022-11-29 14:59:46 -0600

    Se intalaro react-icons se creo un estado para el reminder y una visualizacion de true o false, también un form para el reminder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -23,10 +24,22 @@ function App() {
       reminder: false,
     },
   ])
+
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
   return (
     <div className="container">
       <Header title="Task Tracker" />
-      <Tasks tasks={tasks} />
+      <AddTask/>
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No tasks to show')}
     </div>
   )
 }
