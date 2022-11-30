c20ab260a13565c2fccb836ca1b3b63fc38b2e86
Alex <alexrobertomata@hotmail.com>
2022-11-30 14:52:21 -0600

    Se agregó la funcion de agregar task

--- src/App.js
+++ src/App.js
@@ -25,6 +25,15 @@ function App() {
     },
   ])
 
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    
+    const newTask = { id, ...task }
+    console.log(newTask)
+    setTasks([...tasks, newTask])
+  }
+
   //Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -38,7 +47,7 @@ function App() {
   return (
     <div className="container">
       <Header title="Task Tracker" />
-      <AddTask/>
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No tasks to show')}
     </div>
   )
