fc549dca4560b8ebc0f3bb814d504efa493ab39a
Alex <alexrobertomata@hotmail.com>
2022-12-01 09:50:08 -0600

    Se agregó un estado para mostrar el form de agregar task

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -21,7 +21,7 @@ const AddTask = ({ onAdd }) => {
   }
   
   return (
-    <form className="add-form container" onSubmit={onSubmit}>
+    <form className="add-form" onSubmit={onSubmit}>
       <div className="form-control">
         <label>Task</label>
         <input type="text" placeholder="Add Task" value={text} onChange={(e)=>{setText(e.target.value)}}/>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,29 @@
-import PropTypes from 'prop-types'
-import Button from './Button';
+import PropTypes from "prop-types";
+import Button from "./Button";
 
-const Header = ({ title }) => {
-    const onClick = () => {
-        console.log("click");
-    }
-    return (
-        <header className='header'>
-            <h1>{title}</h1>
-            <Button color='green' text='Hello' onClick={onClick}/>
-        </header>
-    );
+const Header = ({ title, onAdd, showAdd }) => {
+  // const onClick = () => {
+  //     console.log("click");
+  // }
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button
+        color={!showAdd ? "green" : "red"}
+        text={!showAdd ? "Add" : "Close"}
+        onClick={onAdd}
+      />
+    </header>
+  );
 };
 
 Header.defaultProps = {
-    title: 'Task Tracker',
-}
+  title: "Task Tracker",
+};
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
-}
+  title: PropTypes.string.isRequired,
+};
 
 //CSS in JS
 // const headingStyle = {
